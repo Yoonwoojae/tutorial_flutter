aba9befcd37fd7e24cd95334341d39cef4e62c32
woojea <dbsdnwp1004@naver.com>
2019-10-25 10:02:00 +0900

    코멘트 추가

--- lib/snack_bar_1.dart
+++ lib/snack_bar_1.dart
@@ -30,3 +30,18 @@ class SnackBar1 extends StatelessWidget {
     );
   }
 }
+
+//2번째 방법 builder 위젯 사용
+//Scaffold(
+//appBar: AppBar(
+//title: Text('My Profile'),
+//),
+//body: Builder(
+//builder: (ctx) => RaisedButton(
+//textColor: Colors.red,
+//child: Text('Submit'),
+//onPressed: () =>  Scaffold.of(ctx).showSnackBar(SnackBar(content: Text('Profile Save'),
+//),
+//),
+//),
+//);
